202b1605b2115b38b20e90201b596003fe400fe6
JoelCunningham <s3949308@student.rmit.edu.au>
2025-04-08 14:54:19 +1000

    Adds tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-
-const getApps = async (): Promise<{ id: string; name: string; location: string }[]> => {
-    console.log('Fetching apps...');
-    const apps = await window.Electron.ipcRenderer.invoke('get-apps');
-    console.log(apps);
-    return apps;
-};
+import './index.css';
 
 const App = () => {
     const [apps, setApps] = useState<{ id: string; name: string; location: string }[]>([]);
 
     useEffect(() => {
         const fetchApps = async () => {
-            const appsData = await getApps();
+            const appsData = await window.Electron.ipcRenderer.invoke('get-apps');
             setApps(appsData);
         };
         fetchApps();
     }, []);
 
     return (
-        <div>
-            <h2>Hello from React!</h2>
+        <div className='bg-black'>
+            <h1 className="font-bold text-2xl underline text-red-700">Hello react</h1>
             {apps.map((app) => (
                 <div key={app.id}>
                     <h3>{app.name}</h3>
--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,7 +1,16 @@
 import type { Configuration } from 'webpack';
 
-import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
+import { rules } from './webpack.rules';
+
+rules.push({
+  test: /\.css$/,
+  use: [
+    { loader: 'style-loader' },
+    { loader: 'css-loader' },
+    { loader: 'postcss-loader' }
+  ],
+});
 
 export const rendererConfig: Configuration = {
   module: {
