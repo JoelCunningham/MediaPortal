003ab4c588e1ea7052f7d244d1a4708af7fb93c1
JoelCunningham <s3949308@student.rmit.edu.au>
2025-04-10 21:58:34 +1000

    Updates icon to be customisable

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import 'material-symbols'
+import 'material-symbols/rounded.css';
 
-const Icon = ({ icon, size, className }: IconInterface) => (
+const Icon = ({ icon, size, className, filled, weight, grade, optical }: IconInterface) => (
     <span
-        className={`material-symbols-outlined ${className}`}
-        style={{ fontSize: size }}    >
+        className={`material-symbols-rounded ${className}`}
+        style={{ 
+            fontSize: size,
+            fontVariationSettings: `
+                'FILL' ${filled ? '1' : '0'},
+                'wght' ${weight ? weight : '400'},
+                'GRAD' ${grade ? grade : '0'},
+                'opsz' ${optical ? optical : '24'}
+            `,
+
+        }}    >
         {icon}
     </span>
 );
@@ -15,4 +24,8 @@ interface IconInterface {
     icon: string;
     size?: number;
     className?: string;
+    filled?: boolean;
+    weight?: number;
+    grade?: number;
+    optical?: number;
 }
